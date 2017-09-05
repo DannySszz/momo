e85a9e1b2ea89bb3d6e973cbf5c33f96a2155718
DannySszz <31622330+DannySszz@users.noreply.github.com>
2017-09-05 10:40:51 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,6 @@
 	$conn = mysqli_connect("localhost","root","","DBCerita");
 	//testing ubah momo
 	//testing update dari git
-	//testing update dari git2
 	// if($host){
 	// 	echo "koneksi host berhasil.<br/>";
 	// }else{
